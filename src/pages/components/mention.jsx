7af6322c272fb1608a2e3fd0712d820b4dee0b59
Baudouin Dafflon <trysac@gmail.com>
2024-11-06 15:25:39 +0100

    fix header

--- src/pages/components/mention.jsx
+++ src/pages/components/mention.jsx
@@ -22,98 +22,154 @@ export default function Mention() {
         <Grid item md={3} display={{ xs: "none", md: "block",}}>
           <Typography sx={{fontWeight:300, fontSize:"1.5rem",mr:4, color:'orange', textAlign:"right"}}>Mention légales</Typography>
         </Grid>
-        <Grid item xs={12} md={9} align="center" sx={{ mt:5 }}>
+        <Grid item xs={12} md={9} align="center" sx={{ mt:0 }}>
             <Typography sx={{  textAlign:"justify"}}>
-            En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans
-            l’économie numérique, il est précisé aux utilisateurs du site XXX l’identité des
-            différents intervenants dans le cadre de sa réalisation et de son suivi :
-            Maître Victoire DAFFLON, Avocat
-            Siret : XXX
-            43, Avenue Pierre Sémard – 11100 NARBONNE
-            Le responsable publication est une personne physique ou une personne morale.
-            Hébergeur : XXX
+            En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site www.dafflon-avocat.com l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi:
+            
+            <Typography sx={{  textAlign:"justify"}}>
+            Maître Victoire DAFFLON
+            </Typography>
+            <Typography sx={{  textAlign:"justify"}}>
+            43, Avenue Pierre Sémard 11100 Narbonne
+            </Typography>
+            <Typography sx={{  textAlign:"justify"}}>
+            SIRET n° 817 681 018 00021
+            </Typography>
+            Hébergeur : Github.com
             </Typography>
 
-        <Typography sx={{  textAlign:"justify", mt:2}}>
-2. Conditions générales d’utilisation du site et des services proposés.
-L’utilisation du site XXX implique l’acceptation pleine et entière des conditions
-générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont
-susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du
-site XXX sont donc invités à les consulter de manière régulière.
-Ce site est normalement accessible à tout moment aux utilisateurs. Une
-interruption pour raison de maintenance technique peut être toutefois décidée
-par Maître Victoire DAFFLON, qui s’efforcera alors de communiquer
-préalablement aux utilisateurs les dates et heures de l’intervention.
-Le site XXX est mis à jour régulièrement par Maître Victoire DAFFLON. De la
-même façon, les mentions légales peuvent être modifiées à tout moment : elles
-s’imposent néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent
-possible afin d’en prendre connaissance.
-</Typography>
-<Typography sx={{  textAlign:"justify", mt:2}}>
-3. Description des services fournis.
-Le site XXX a pour objet de fournir une information concernant l’ensemble des
-activités de Maître Victoire DAFFLON.
-Maître Victoire DAFFLON s’efforce de fournir sur le site XXX des informations
-aussi précises que possible. Toutefois, il ne pourra être tenue responsable des
-omissions, des inexactitudes et des carences dans la mise à jour, qu’elles soient
-de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.
-Tous les informations indiquées sur le XXX sont données à titre indicatif, et sont
-susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site XXX ne
-sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été
-apportées depuis leur mise en ligne.
+        <Typography sx={{  textAlign:"justify", mt:2}}>2. Conditions générales d’utilisation du site et des services proposés.</Typography>
+        <Typography sx={{  textAlign:"justify"}}>L’utilisation du site www.dafflon-avocat.com implique l’acceptation pleine et entière des
+        conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont
+        susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du site
+        www.dafflon-avocat.com sont donc invités à les consulter de manière régulière.
+        Ce site est normalement accessible à tout moment aux utilisateurs. Une interruption pour
+        raison de maintenance technique peut être toutefois décidée par Maître Victoire DAFFLON,
+        qui s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de
+        l’intervention. Le site www.dafflon-avocat.com est mis à jour régulièrement par Maître
+        Victoire DAFFLON. De la même façon, les mentions légales peuvent être modifiées à tout
+        moment : elles s’imposent néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent
+        possible afin d’en prendre connaissance.
+        </Typography>
+<Typography sx={{  textAlign:"justify", mt:2}}>3. Description des services fournis.</Typography>
+<Typography sx={{  textAlign:"justify"}}>Le site www.dafflon-avocat.com a pour objet de fournir une information concernant
+l’ensemble des activités de la société.
+Maître Victoire DAFFLON s’efforce de fournir sur le site www.dafflon-avocat.com des
+informations aussi précises que possible. Toutefois, elle ne pourra être tenue responsable des
+omissions, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou
+du fait des tiers partenaires qui lui fournissent ces informations.
+Tous les informations indiquées sur le site www.dafflon-avocat.com sont données à titre
+indicatif, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site
+www.dafflon-avocat.com ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications
+ayant été apportées depuis leur mise en ligne.
 </Typography>
 <Typography sx={{  textAlign:"justify", mt:2}}> 
-4. Limitations contractuelles sur les données techniques.
-Le site Internet ne pourra être tenu responsable de dommages matériels liés à
-l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en
-
-utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de
-dernière génération mis-à-jour.
+4. Limitations contractuelles sur les données techniques.</Typography>
+<Typography sx={{  textAlign:"justify"}}>Le site utilise la technologie JavaScript.
+Le site Internet ne pourra être tenu responsable de dommages matériels liés à l’utilisation du
+site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne
+contenant pas de virus et avec un navigateur de dernière génération mis-à-jour.
 </Typography>
-​<Typography sx={{  textAlign:"justify", mt:2}}> 
-5. Propriété intellectuelle et contrefaçons.
-Maître Victoire DAFFLON est propriétaire des droits de propriété intellectuelle ou
-détient les droits d’usage sur tous les éléments accessibles sur le site.
-Toute reproduction, représentation, modification, publication, adaptation de
-tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé,
-est interdite, sauf autorisation écrite préalable de : Maître Victoire DAFFLON.
-Toute exploitation non autorisée du site ou de l’un quelconque des éléments
-qu’il contient sera considérée comme constitutive d’une contrefaçon et
-poursuivie conformément aux dispositions des articles L.335-2 et suivants du
+<Typography sx={{  textAlign:"justify", mt:2}}> 
+5. Propriété intellectuelle et contrefaçons.</Typography>
+<Typography sx={{  textAlign:"justify"}}> 
+Maître Victoire DAFFLON est propriétaire des droits de propriété intellectuelle ou détient les
+droits d’usage sur tous les éléments accessibles sur le site.
+Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des
+éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation
+écrite préalable de Maître Victoire DAFFLON. Toute exploitation non autorisée du site ou de
+l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une
+contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du
 Code de Propriété Intellectuelle.
 </Typography>
-​<Typography sx={{  textAlign:"justify", mt:2}}> 
-6. Limitations de responsabilité.
-Maître Victoire DAFFLON ne pourra être tenue responsable des dommages
-directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site XXX,
-et résultant soit de l’utilisation d’un matériel ne répondant pas aux spécifications
-indiquées au point 4, soit de l’apparition d’un bug ou d’une incompatibilité.
-Maître Victoire DAFFLON ne pourra également être tenue responsable des
-dommages indirects (tels par exemple qu’une perte de marché ou perte d’une
-chance) consécutifs à l’utilisation du site XXX.
-</Typography>
 <Typography sx={{  textAlign:"justify", mt:2}}> 
-7. Gestion des données personnelles.
-En France, les données personnelles sont notamment protégées par la loi n° 78-
-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l’article L. 226-13 du Code
-pénal et la Directive Européenne du 24 octobre 1995.
-Ce site ne demande pas d&#39;enregistrement nominatif à ses visiteurs et ne procède
-à aucun enregistrement nominatif.
-8. Liens hypertextes et cookies.
-Les liens hypertextes présentés sur le site en direction de ressources extérieures
-ne sauraient engager la responsabilité de Maître Victoire DAFFLON.
-La création de liens hypertextes vers le présent site ne peut se faire sans
-l’autorisation expresse et préalable de Maître Victoire DAFFLON.
+6. Limitations de responsabilité.</Typography>
+<Typography sx={{  textAlign:"justify"}}>Maître Victoire DAFFLON ne pourra être tenue responsable des dommages directs et
+indirects causés au matériel de l’utilisateur, lors de l’accès au site www.dafflon-avocat.com, et
+résultant soit de l’utilisation d’un matériel ne répondant pas aux spécifications indiquées au
+point 4, soit de l’apparition d’un bug ou d’une incompatibilité. Maître Victoire DAFFLON ne
+pourra également être tenue responsable des dommages indirects (tels par exemple qu’une
+perte de marché ou perte d’une chance) consécutifs à l’utilisation du site www.dafflon-
+avocat.com. Des espaces interactifs sont à la disposition des utilisateurs. Maître Victoire
+DAFFLON se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu
+déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier
+aux dispositions relatives à la protection des données. Le cas échéant, elle se réserve
+également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur,
+notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique,
+quel que soit le support utilisé (texte, photographie...).
 </Typography>
-<Typography sx={{  textAlign:"justify", mt:2}}> 
-9. Droit applicable et attribution de juridiction.
-Tout litige en relation avec l’utilisation du site XXX est soumis au droit français. Il
-est fait attribution exclusive de juridiction aux tribunaux compétents de Paris.
-10. Les principales lois concernées.
-Loi n° 78-17 du 6 janvier 1978, notamment modifiée par la loi n° 2004-801 du 6
-août 2004 relative à l’informatique, aux fichiers et aux libertés.
+
+<Typography sx={{  textAlign:"justify", mt:2}}>7. Gestion des données personnelles.</Typography>
+<Typography sx={{  textAlign:"justify"}}>En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier
+1978, la loi n° 2004-801 du 6 août 2004, l’article L. 226-13 du Code pénal et la Directive
+Européenne du 24 octobre 1995. A l’occasion de l’utilisation du site www.dafflon-
+avocat.com, peuvent être recueillies : l’URL des liens par l’intermédiaire desquels l’utilisateur
+a accédé au site www.dafflon-avocat.com, le fournisseur d’accès de l’utilisateur, l’adresse de
+protocole Internet (IP) de l’utilisateur. En tout état de cause Maître Victoire DAFFLON ne
+collecte des informations personnelles relatives à l’utilisateur que pour le besoin de certains
+services proposés par le site www.dafflon-avocat.com. L’utilisateur fournit ces informations
+en toute connaissance de cause, notamment lorsqu’il procède par lui-même à leur saisie. Il est
+alors précisé à l’utilisateur du site www.dafflon-avocat.com l’obligation ou non de fournir ces
+informations. Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6
+janvier 1978 relative à l’informatique, aux fichiers et aux libertés, tout utilisateur dispose d’un
+droit d’accès, de rectification et d’opposition aux données personnelles le concernant, en
+effectuant sa demande écrite et signée, accompagnée d’une copie du titre d’identité avec
+signature du titulaire de la pièce, en précisant l’adresse à laquelle la réponse doit être envoyée.
+
+Aucune information personnelle de l’utilisateur du www.dafflon-avocat.com n’est publiée à
+l’insu de l’utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des
+tiers. Seule l’hypothèse du rachat de Maître Victoire DAFFLON et de ses droits permettrait la
+transmission des dites informations à l’éventuel acquéreur qui serait à son tour tenu de la
+même obligation de conservation et de modification des données vis à vis de l’utilisateur du
+site www.dafflon-avocat.com. Le site n’est pas déclaré à la CNIL car il ne recueille pas
+d’informations personnelles.
+Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998
+transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de
+données.
+</Typography>
+<Typography sx={{  textAlign:"justify", mt:2}}>
+  8. Liens hypertextes et cookies.</Typography>
+  <Typography sx={{  textAlign:"justify"}}>Le site www.dafflon-avocat.com contient un certain nombre de liens hypertextes vers d’autres
+sites, mis en place avec l’autorisation de Maître Victoire DAFFLON. Cependant, elle n’a pas
+la possibilité de vérifier le contenu des sites ainsi visités, et n’assumera en conséquence
+aucune responsabilité de ce fait.
+La navigation sur le site www.dafflon-avocat.com est susceptible de provoquer l’installation
+de cookie(s) sur l’ordinateur de l’utilisateur. Un cookie est un fichier de petite taille, qui ne
+permet pas l’identification de l’utilisateur, mais qui enregistre des informations relatives à la
+navigation d’un ordinateur sur un site. Les données ainsi obtenues visent à faciliter la
+navigation ultérieure sur le site, et ont également vocation à permettre diverses mesures de
+fréquentation.
+Le refus d’installation d’un cookie peut entraîner l’impossibilité d’accéder à certains services.
+L’utilisateur peut toutefois configurer son ordinateur de la manière suivante, pour refuser
+l’installation des cookies :
+Sous Internet Explorer : onglet outil (pictogramme en forme de rouage en haut a droite) /
+options internet. Cliquez sur Confidentialité et choisissez Bloquer tous les cookies. Validez
+sur Ok.
+Sous Firefox : en haut de la fenêtre du navigateur, cliquez sur le bouton Firefox, puis aller
+dans l’onglet Options. Cliquer sur l’onglet Vie privée.
+Paramétrez les Règles de conservation sur : utiliser les paramètres personnalisés pour
+l’historique. Enfin décochez-la pour désactiver les cookies.
+Sous Safari : Cliquez en haut à droite du navigateur sur le pictogramme de menu (symbolisé
+par un rouage). Sélectionnez Paramètres. Cliquez sur Afficher les paramètres avancés. Dans la
+section « Confidentialité », cliquez sur Paramètres de contenu. Dans la section « Cookies »,
+vous pouvez bloquer les cookies.
+Sous Chrome : Cliquez en haut à droite du navigateur sur le pictogramme de menu (symbolisé
+par trois lignes horizontales). Sélectionnez Paramètres. Cliquez sur Afficher les paramètres
+avancés. Dans la section « Confidentialité », cliquez sur préférences. Dans l’onglet
+« Confidentialité », vous pouvez bloquer les cookies.
+</Typography>
+<Typography sx={{  textAlign:"justify", mt:2}}>9. Droit applicable et attribution de juridiction.</Typography>
+<Typography sx={{  textAlign:"justify"}}>
+  Tout litige en relation avec l’utilisation du site www.dafflon-avocat.com est soumis au droit
+  français. Il est fait attribution exclusive de juridiction aux tribunaux compétents de Paris.
+  </Typography>
+  <Typography sx={{  textAlign:"justify", mt:2}}>
+    10. Les principales lois concernées.</Typography>
+    <Typography sx={{  textAlign:"justify"}}>Loi n° 78-17 du 6 janvier 1978, notamment modifiée par la loi n° 2004-801 du 6 août 2004
+relative à l’informatique, aux fichiers et aux libertés.
 Loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique.
-          </Typography>
+</Typography>
+           
            
         </Grid>
          
