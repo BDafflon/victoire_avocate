7e8ca4d742f6d2ce40641f3d11d2a32179be446e
BDafflon <trysac@gmail.com>
2024-11-17 12:02:05 +0100

    add maps

--- src/pages/components/sitemap.jsx
+++ src/pages/components/sitemap.jsx
@@ -1,7 +1,7 @@
 let maps = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <lochttps://dafflon-avocat.com/</loc>
+    <loc>https://dafflon-avocat.com/</loc>
     <lastmod>2024-11-15</lastmod>
     <changefreq>daily</changefreq>
     <priority>1</priority>
