3d33c85d7fefbe25394407327cbbb4fa8b676fe8
BDafflon <trysac@gmail.com>
2024-11-17 22:07:38 +0100

    add param3

--- src/pages/components/sitemap.jsx
+++ src/pages/components/sitemap.jsx
@@ -7,37 +7,37 @@ let maps = `<?xml version="1.0" encoding="UTF-8"?>
     <priority>1</priority>
   </url>
   <url>
-    <loc>https://dafflon-avocat.com/lecabinet</loc>
+    <loc>https://dafflon-avocat.com/?page_id=lecabinet</loc>
     <lastmod>2024-11-15</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
     <url>
-    <loc>https://dafflon-avocat.com/domainesdecompetences</loc>
+    <loc>https://dafflon-avocat.com/?page_id=domainesdecompetences</loc>
     <lastmod>2024-11-15</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
     <url>
-    <loc>https://dafflon-avocat.com/honoraire</loc>
+    <loc>https://dafflon-avocat.com/?page_id=honoraire</loc>
     <lastmod>2024-11-15</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
     <url>
-    <loc>https://dafflon-avocat.com/contact</loc>
+    <loc>https://dafflon-avocat.com/?page_id=contact</loc>
     <lastmod>2024-11-15</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
     <url>
-    <loc>https://dafflon-avocat.com/mention</loc>
+    <loc>https://dafflon-avocat.com/?page_id=mention</loc>
     <lastmod>2024-11-15</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
    <url>
-    <loc>https://dafflon-avocat.com/information</loc>
+    <loc>https://dafflon-avocat.com/?page_id=information</loc>
     <lastmod>2024-11-15</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
