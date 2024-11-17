b2f7ecbf95ca1dd37867288f927aaa8d38b1b1cc
BDafflon <trysac@gmail.com>
2024-11-17 21:39:58 +0100

    fix routeur4

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -265,7 +265,7 @@ export default function HomePage({ page_id }) {
                     color: page == 3 ? "orange" : "black",
                     "&:hover": { color: "orange" },
                   }}
-                  onClick={() => navigate('/contact')}
+                  onClick={() => navigate('contact')}
                 >
                   Contact
                 </Link>
